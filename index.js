37cc82c5d01084896d0f027769401151fa6aed38
arkan <shibarkan01@gmail.com>
2025-06-29 05:19:34 +0700

    asd

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello express test"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(8080, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
